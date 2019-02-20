f0df806af6beeeb98219e1151054464d46f07c53
Mairz <807323714@qq.com>
2019-02-20 15:03:58 +0800

    Update function.php

--- function.php
+++ function.php
@@ -62,6 +62,37 @@ function testwrite($d){
     }
 }
 
+//创建文件夹
+function dir_create($path,$mode=0777){
+    if(is_dir($path)){
+        return true;
+    }
+    $ftp_enable = 0;
+    $path = dir_path($path);
+    $temp = explode("/",$path);
+    $cur_dir = "";
+    $max = count($temp) - 1;
+    for($i = 0;$i<$max;$i++){
+        $cur_dir .= $temp[$i]."/";
+        if(@is_dir($cur_dir)){
+            continue;
+        }
+        @mkdir($cur_dir,0777,true);
+        @chmod($cur_dir,0777);
+    }
+    return is_dir($path);
+
+}
+
+
+function dir_path($path){
+   $path = str_replace("\\","/",$path);
+   if(substr($path,-1) != "/"){
+       $path = $path."/";
+   }
+   return $path;
+}
+
 
 
 //sql执行篇
