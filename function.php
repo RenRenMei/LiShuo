f818ab2866cdf17d7d8a0fc8e93755369edb1ccf
Mairz <807323714@qq.com>
2019-02-20 10:48:08 +0800

    Update function.php

--- function.php
+++ function.php
@@ -35,3 +35,36 @@ function delFile($dir,$file_type="")
     }
 
 }
+
+
+//sql执行篇
+//批量执行.sql中的sql语句    
+function sql_split($sql, $tablepre) {
+
+    if ($tablepre != "tp_")
+    	$sql = str_replace("tp_", $tablepre, $sql);
+          
+    $sql = preg_replace("/TYPE=(InnoDB|MyISAM|MEMORY)( DEFAULT CHARSET=[^; ]+)?/", "ENGINE=\\1 DEFAULT CHARSET=utf8", $sql);
+    
+    $sql = str_replace("\r", "\n", $sql);
+    $ret = array();
+    $num = 0;
+    $queriesarray = explode(";\n", trim($sql));
+    unset($sql);
+    foreach ($queriesarray as $query) {
+        $ret[$num] = '';
+        $queries = explode("\n", trim($query));
+        $queries = array_filter($queries);
+        foreach ($queries as $query) {
+            $str1 = substr($query, 0, 1);
+            if ($str1 != '#' && $str1 != '-')
+                $ret[$num] .= $query;
+        }
+        $num++;
+    }
+    return $ret;
+}
+
+
+
+
